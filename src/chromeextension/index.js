5330a47f1fc08f154ef8fafba8370060216d8a2e
yangxinxiang <yangxinxiang@100tal.com>
2021-12-06 22:23:55 +0800

    feat: 添加chrome extensionn扩展实现提取房产信息

---
+++ src/chromeextension/index.js
@@ -0,0 +1,193 @@
+﻿//自动调用接口禁用海尔账号
+
+let hasDrawTable = false;
+let myApp = null;
+//输出日志
+function outputLog(str){
+	var outputPanel = $id("outputPanel");
+	if(outputPanel){
+		outputPanel.value +=str+"\n";
+	}else{
+		console.log(str);
+	}	
+}
+
+outputLog("hello world");
+
+//var _info = "Data Loaded. ID Length = ["+window.UserIDList.length+"] , Email length = ["+window.EmailList.length+"]";
+
+//outputLog(_info);
+
+//封装常用的dom类库
+function $id(id){
+	return document.getElementById(id);
+}
+function $name(name){
+	return document.getElementsByName(name);
+}
+
+
+var info = "";
+function ajaxGet(url) {
+	var xhr = new XMLHttpRequest();
+	xhr.onreadystatechange = function () {		
+		if(xhr.readyState==4){
+			resultHandler(xhr.responseText);
+		}
+	}
+	xhr.open("GET", url, true);
+	xhr.send(null);
+}
+
+
+
+//响应按钮点击
+function start(){
+	outputLog("start :: enter.");
+	if(myApp) {
+		myApp.startParse()
+	}
+	
+}
+//响应按钮点击
+function stop(){
+	outputLog("stop :: enter.");
+	
+}
+
+//注入添加操作界面
+function addUserPanel(){
+	var d= document;
+	//var oFragment = d.createDocumentFragment();
+	//创建外层div
+	var outerDiv = d.createElement("div");
+	outerDiv.style.position="fixed";
+	outerDiv.style.top="50px";
+	outerDiv.style.left="160px";
+	outerDiv.style.border="1px solid #7ba0c3";
+	outerDiv.style.width="240px";
+	//outerDiv.style.height="150px";
+	outerDiv.style.padding="5px"
+	//创建说明提示
+	var infoTips = d.createElement("span");
+	infoTips.style.fontSize="14px";
+	infoTips.innerHTML="如果界面上部分数据还没有显示，请尝试点击【分析页面】按钮再次分析";
+	outerDiv.appendChild(infoTips);
+	
+	var This = this;
+	
+	/////////////////创建更人性化的提示框
+	outerDiv.appendChild(d.createElement("br"));
+	outerDiv.appendChild(d.createElement("br"));
+	var b1 = d.createElement("input");
+	b1.type="button";
+	b1.name="startBuuton";
+	b1.id="startBuuton";
+	b1.value = "分析页面";
+	outerDiv.appendChild(b1);
+	outerDiv.appendChild(d.createElement("br"));
+	b1.onclick=function(){
+		start();
+	};	
+	// 添加到文档树
+	document.body.appendChild(outerDiv);
+	
+	addTable()
+	
+}
+
+function createTableStyle(){
+	const styles = document.createElement("style");
+	styles.innerHTML = `
+	table.gridtable { 
+        font-family: verdana,arial,sans-serif; font-size:11px; color:#333333; border-width: 1px; border-color: #666666; border-collapse: collapse; position:fixed;top:"160px";left:"300px"
+    } 
+    table.gridtable th 
+    { border-width: 1px; padding: 8px; border-style: solid; border-color: #666666; background-color: #dedede; } 
+	table.gridtable th:first-child {width:100px}
+    table.gridtable td { border-width: 1px; padding: 8px; border-style: solid; border-color: #666666; background-color: #ffffff; } `
+	document.head.appendChild(styles)
+}
+
+function addTable() {
+	var outputArea = document.createElement("div");
+	outputArea.id = "outputPanel";	
+	outputArea.style.position="fixed";
+	outputArea.style.top="200px";
+	outputArea.style.left="30px";
+	outputArea.innerHTML = `<table class="gridtable"> 
+        <tr> 
+            <th v-for="item in myHeaders">{{item}}</th> 
+        </tr>
+		
+		<tr v-for="hourse in myHourseInfos">
+			<td  v-for="item in hourse">{{item}}</td>
+		<tr/>
+       
+    </table>`
+																					
+	document.body.appendChild(outputArea);
+}
+
+function startVueApp() {
+	console.log(`myExtension startVueApp :: enter, hasDrawTable = ${hasDrawTable}`)
+	if(hasDrawTable) {
+		console.log(`myExtension startVueApp :: enter, hasDrawTable = ${hasDrawTable}`)
+	}
+	myApp = new Vue({
+		el: '#outputPanel',
+		data: {  		  																				
+		  	myHeaders:["标题","面积（㎡）", "房间类型", "朝向", "楼层", "添加时候的报价（万）", "装修情况", "网上说的税费（万）", "预计总首付（不含中介费）", "元/㎡", "小区均价", "挂牌时间", "关注时间", "购买意愿", "是否已现场看", "是否已成交", "是否有电梯", "梯户比", "建筑年代", "持有年数",  "抵押贷款","备注", "房源连接", "其他补充说明"],
+			myHourseInfos : []
+		},
+        mounted: function () {            
+			console.log(`myExtension mounted :: enter.`)
+			this.startParse();
+        },
+		methods: {
+			startParse() {
+				console.log(`myExtension startParse :: enter.`)
+				const data = parse(document.documentElement.outerHTML);
+				const {title, area, hourseType, orientation, floor, price, fitUp, taxation, totalDownPayment, unitPrice, averagePrice, listingTime, hasElevator, tiHuBi, buildYear, rid, holdYears, mortgageInfo} = data
+				console.log(`myExtension startParse :: title = ${title}`)
+				const followDate = this.getDateStr(new Date());  // "关注时间"
+				const willing = ""; // "购买意愿"
+				const hasViewed = false; // "是否已现场看"
+				const hasClosed = false; // "是否已成交"
+				const remarks = ""; // 备注
+				const url = location.href;
+				const others = ""
+				const hourseInfo = [title, area, hourseType, orientation, floor, price, fitUp, taxation, totalDownPayment, unitPrice, averagePrice, listingTime, 
+					followDate,
+					willing,
+					hasViewed,
+					hasClosed,
+					hasElevator, tiHuBi, buildYear, holdYears, mortgageInfo, remarks, url, others];
+				this.myHourseInfos.push(hourseInfo);
+			},
+			getDateStr(date){
+				const yyyy = date.getFullYear()
+				const mm = (date.getMonth()+1) + "";
+				const dd =  date.getDate()+ "";
+				return `${yyyy}.${mm.padStart(2,"0")}.${dd.padStart(2,"0")}`
+			}
+		}
+	  })
+	  hasDrawTable = true
+}
+//暂时将程序注入到变更管理页面的登陆页面中
+var url = location.href;
+// if(url.indexOf("https://bj.lianjia.com")>=0){
+if(url.indexOf("mofaxiao.com")>=0 || url.indexOf("https://bj.lianjia.com")>=0){
+	// 很奇怪，会执行两遍。。。。暂时没找到解决方案。
+	createTableStyle(); //创建表格样式
+	addUserPanel();
+	startVueApp()
+	
+}
+
+document.addEventListener("load", ()=> {
+	console.log(`myExtension loaded :: enter.`)
+	// startVueApp()
+})
+
---
+++ src/chromeextension/parser.js
@@ -0,0 +1,402 @@
+function parse(pageStr) {
+    // console.log(pageStr)
+
+    const title = getTitle(pageStr) //document.querySelector(".total").innerHTML; // 标题
+    const area = getArea(pageStr) // document.querySelector(".unitPriceValue").innerText.replace("元/平米", "");  // 面积（㎡）
+    const hourseType = getHourseType(pageStr) // document.querySelector(".base .content ul li").innerText.replace("房屋户型", "");  // 房间类型
+    const orientation = getOrientation(pageStr) // document.querySelector(".base .content ul").children[6].innerText.replace("房屋朝向", "");  // 朝向
+    const floor = getFloor(pageStr) // document.querySelector(".base .content ul").children[1].innerText.replace("所在楼层", ""); // 楼层
+    const price = getPrice(pageStr) // document.querySelector(".total").innerText;  // 总价
+    const fitUp = getFitUp(pageStr) // 获取装修程度
+    const taxation = getTaxation(pageStr) // document.querySelector(".number").innerText;  // 添加时候的报价（万）
+    const totalDownPayment = getTotalDownPayment(pageStr);  // 总首付
+    const unitPrice = getUnitPrice(pageStr);  // 元/㎡
+    const averagePrice = getAveragePrice(pageStr);  // 小区均价
+    const listingTime = getListingTime(pageStr);  // 挂牌时间
+    const hasElevator = getHasElevator(pageStr);  // 是否有电梯
+    const tiHuBi = getTiHuBi(pageStr);  // 梯户比
+    const buildYear = getBuildYear(pageStr);  // 建筑年代
+    const holdYears = getHoldYears(pageStr); // 是否满5年，满2年
+    const mortgageInfo = getMortgageInfo(pageStr) // 房屋抵押信息
+    const rid = getRid(pageStr) // 小区id
+    // const url = "";  // 房源连接
+    console.log(title, area, hourseType, orientation, floor, price, fitUp, taxation, totalDownPayment, unitPrice, averagePrice, listingTime, hasElevator, tiHuBi, buildYear, rid, holdYears, mortgageInfo)
+    return {title, area, hourseType, orientation, floor, price, fitUp, taxation, totalDownPayment, unitPrice, averagePrice, listingTime, hasElevator, tiHuBi, buildYear, rid, holdYears, mortgageInfo}
+}
+/**
+ * 获取房子的主题描述
+ * @param {*} pageStr 
+ * @param {*} from 
+ * @returns 
+ */
+function getTitle(pageStr, from = "LJ") {
+    // dom 格式 <span class="total">570</span><
+    const reg = /<title>{1}(.+)<\/title>/;
+    const rst = reg.exec(pageStr)
+    if(rst && rst[1]) {
+        return rst[1]
+    } else {
+        return -1;
+    }
+}
+/**
+ * 获取面积
+ * @param {*} pageStr 
+ * @param {*} from 
+ * @returns 
+ */
+function getArea(pageStr, from = "LJ") {
+    // dom 格式 <div class="area"><div class="mainInfo">86.39平米</div>
+    const reg = /<div class="area"><div class="mainInfo">{1}(.+)平米/;
+    const rst = reg.exec(pageStr)
+    if(rst && rst[1]) {
+        return rst[1]
+    } else {
+        return -1;
+    }
+}
+/**
+ * 获取房间的朝向类型
+ * @param {*} pageStr 
+ * @param {*} from 
+ * @returns 
+ */
+function getHourseType(pageStr, from = "LJ") {
+    // dom 格式 <div class="room"><div class="mainInfo">2室1厅</div>
+    const reg = /<div class="room"><div class="mainInfo">{1}(.{4,10})<\/div>/;
+    const rst = reg.exec(pageStr)
+    if(rst && rst[1]) {
+        return rst[1]
+    } else {
+        return -1;
+    }
+}
+
+/**
+ * 获取房间的朝向类型
+ * @param {*} pageStr 
+ * @param {*} from 
+ * @returns 
+ */
+ function getOrientation(pageStr, from = "LJ") {
+    // dom 格式 <div class="type"><div class="mainInfo" title="南 北">南 北</div>
+    const reg = /<div class="type"><div class="mainInfo" title="{1}(.{2,6})">/;
+    const rst = reg.exec(pageStr)
+    if(rst && rst[1]) {
+        return rst[1]
+    } else {
+        return -1;
+    }
+}
+
+/**
+ * 获取房间的朝向类型
+ * @param {*} pageStr 
+ * @param {*} from 
+ * @returns 
+ */
+ function getFloor(pageStr, from = "LJ") {
+    // dom 格式 <div class="subInfo">中楼层/共14层</div></div>
+    const reg = /<div class="subInfo">{1}(.{3,12})<\/div>/;
+    const rst = reg.exec(pageStr)
+    if(rst && rst[1]) {
+        return rst[1]
+    } else {
+        return -1;
+    }
+}
+
+/**
+ * 获取总价
+ * @param {*} pageStr 
+ * @param {*} from 
+ * @returns 
+ */
+ function getPrice(pageStr, from = "LJ") {
+    // dom 格式 <div class="price "><span class="total">570</span><span class="unit"></span>
+    const reg = /<div class="price "><span class="total">{1}(.{3,6})<\/span>/;
+    const rst = reg.exec(pageStr)
+    if(rst && rst[1]) {
+        return rst[1]
+    } else {
+        return -1;
+    }
+}
+
+/**
+ * 获取装修程度
+ * @param {*} pageStr 
+ * @param {*} from 
+ * @returns 
+ */
+ function getFitUp(pageStr, from = "LJ") {
+    // dom 格式 <div class="subInfo">平层/精装</div></div><div class="area">
+    const reg = /<div class="subInfo">{1}(.{3,10})<\/div>/;
+    const rst = reg.exec(pageStr)
+    if(rst && rst[1]) {
+        return rst[1]
+    } else {
+        return -1;
+    }
+}
+/**
+ * 获取税费，有点小复杂哈，哈哈
+ * @param {*} pageStr 
+ * @param {*} from 
+ * @returns 
+ */
+ function getTaxation(pageStr, from = "LJ") {
+    // dom 格式 
+    /*
+    <div class="item">
+      <div class="label-title">税费合计</div>
+      <div class="content">
+        
+        <span class="number">50.1</span><span class="unit">万元</span>
+        
+      </div>
+    </div>
+    */
+    const reg = /<div class="label-title">税费合计<\/div>{1}\s+<div class="content">{1}\s+<span class="number">{1}(.{3,6})<\/span>/;
+    const rst = reg.exec(pageStr)
+    if(rst && rst[1]) {
+        return rst[1]
+    } else {
+        return -1;
+    }
+}
+
+/**
+ * 获取总首付
+ * @param {*} pageStr 
+ * @param {*} from 
+ * @returns 
+ */
+ function getTotalDownPayment(pageStr, from = "LJ") {
+    // dom 格式 
+    /*
+    <div class="shoufu-item item">
+      <div class="label-title">首付总计</div>
+      <div class="green content"><span class="number">268.1</span><span class="unit">万元</span></div>
+    </div>
+    */
+    const reg = /<div class="label-title">首付总计<\/div>{1}\s+<div class="green content"><span class="number">{1}(.{3,6})<\/span>/;
+    const rst = reg.exec(pageStr)
+    if(rst && rst[1]) {
+        return rst[1]
+    } else {
+        return -1;
+    }
+}
+
+/**
+ * 获取每平米的价格
+ * @param {*} pageStr 
+ * @param {*} from 
+ * @returns 
+ */
+function getUnitPrice(pageStr, from = "LJ") {
+    // dom 格式 
+    /*
+    <div class="unitPrice"><span class="unitPriceValue">65980<i>元/平米</i></span></div>
+    */
+    const reg = /<div class="unitPrice"><span class="unitPriceValue">{1}(.{3,8})<i>元/;
+    const rst = reg.exec(pageStr)
+    if(rst && rst[1]) {
+        return rst[1]
+    } else {
+        return -1;
+    }
+}
+
+
+
+/**
+ * 获取小区均价，这个不一定有
+ * @param {*} pageStr 
+ * @param {*} from 
+ * @returns 
+ */
+ function getAveragePrice(pageStr, from = "LJ") {
+    // dom 格式 
+    /*
+    <div class="xiaoqu_info">
+            <label class="xiaoqu_main_label">小区均价</label>
+            <span class="xiaoqu_main_info price_red">
+              
+                68652 元/㎡
+              
+            </span>
+          </div>
+    */
+    const reg = /<label class="xiaoqu_main_label">小区均价<\/label>{1}\s+<span class="xiaoqu_main_info price_red">{1}\s+(\d+)/;
+    const rst = reg.exec(pageStr)
+    if(rst && rst[1]) {
+        return rst[1]
+    } else {
+        return -1;
+    }
+}
+
+
+/**
+ * 获取房子的挂牌时间
+ * @param {*} pageStr 
+ * @param {*} from 
+ * @returns 
+ */
+ function getListingTime(pageStr, from = "LJ") {
+    // dom 格式 
+    /*
+    <span class="label ">挂牌时间</span>
+                              <span>2021-11-04</span>
+    */
+    const reg = />挂牌时间<\/span>{1}\s+<span>(.{10})/;
+    const rst = reg.exec(pageStr)
+    if(rst && rst[1]) {
+        return rst[1]
+    } else {
+        return -1;
+    }
+}
+
+
+
+
+
+
+/**
+ * 获取是否有电梯
+ * @param {*} pageStr 
+ * @param {*} from 
+ * @returns 
+ */
+ function getHasElevator(pageStr, from = "LJ") {
+    // dom 格式 
+    /*
+    <li><span class="label">配备电梯</span>有</li>
+    */
+    const reg = /<span class="label">配备电梯<\/span>{1}(.{1,4})<\/li>/;
+    const rst = reg.exec(pageStr)
+    if(rst && rst[1]) {
+        return rst[1]
+    } else {
+        return -1;
+    }
+}
+
+/**
+ * 获取梯户比
+ * @param {*} pageStr 
+ * @param {*} from 
+ * @returns 
+ */
+ function getTiHuBi(pageStr, from = "LJ") {
+    // dom 格式 
+    /*
+    <li><span class="label">梯户比例</span>两梯九户</li>
+    */
+    const reg = /<span class="label">梯户比例<\/span>{1}(.{1,6})<\/li>/;
+    const rst = reg.exec(pageStr)
+    if(rst && rst[1]) {
+        return rst[1]
+    } else {
+        return -1;
+    }
+}
+
+
+/**
+ * 获取建筑年代
+ * @param {*} pageStr 
+ * @param {*} from 
+ * @returns 
+ */
+ function getBuildYear(pageStr, from = "LJ") {
+    // dom 格式 
+    /*
+    <div class="xiaoqu_info">
+              <label class="xiaoqu_main_label">建筑年代</label>
+              <span class="xiaoqu_main_info">1989年</span>
+            </div>
+    
+    <div class="subInfo noHidden">1986年建
+    */
+    const reg = /(\d{4})年建/;
+    const rst = reg.exec(pageStr)
+    if(rst && rst[1]) {
+        return rst[1]
+    } else {
+        return -1;
+    }
+}
+
+
+/**
+ * 获取抵押信息
+ * @param {*} pageStr 
+ * @param {*} from 
+ * @returns 
+ */
+ function getMortgageInfo(pageStr, from = "LJ") {
+    // dom 格式 
+    /*
+   <span class="label">抵押信息</span>
+                              <span style="display:inline-block;width:64%;white-space:nowrap;overflow:hidden;text-overflow:ellipsis;vertical-align:middle;" title="有抵押 120万元 客户偿还">
+                                有抵押 120万元 客户偿还
+                              </span>
+    */
+    // const reg = /<span class="label">抵押信息<\/span>{1}\s+<span\.+\s+>(.{2,20})<\/span>/;
+    const reg = /title="(.+抵押{1,5}.{0,70})">/;
+    const rst = reg.exec(pageStr)
+    if(rst && rst[1]) {
+        return rst[1]
+    } else {
+        return -1;
+    }
+}
+
+
+/**
+ * 获取小区ID
+ * @param {*} pageStr 
+ * @param {*} from 
+ * @returns 
+ */
+ function getRid(pageStr, from = "LJ") {
+    // dom 格式 
+    /*
+    data-lj_action_resblock_id="101113523653" data-lj_action_housedel_id="1111027374324">
+    */
+    const reg = /data-lj_action_housedel_id={1}(\d+)>/;
+    const rst = reg.exec(pageStr)
+    if(rst && rst[1]) {
+        return rst[1]
+    } else {
+        return -1;
+    }
+}
+
+
+/**
+ * 获取小区ID
+ * @param {*} pageStr 
+ * @param {*} from 
+ * @returns 
+ */
+ function getHoldYears(pageStr, from = "LJ") {
+    // dom 格式 
+    /*
+    <span class="label ">房屋年限</span>
+                              <span>满两年</span>
+    */
+    const reg = /<span class="label ">房屋年限<\/span>{1}\s+<span>(.{2,10})<\/span>/;
+    const rst = reg.exec(pageStr)
+    if(rst && rst[1]) {
+        return rst[1]
+    } else {
+        return -1;
+    }
+}
+
+window.parse = parse;
